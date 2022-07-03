6cc62022b077e7e55d20a7b8d7a2e7cb83240e9f
Stenor <stenor0515@gmail.com>
2022-07-03 07:00:15 -0700

    redeploy

---
+++ config.js
@@ -0,0 +1,6 @@
+
+const nftmarketaddress = "0xb5cbd0523612b971EED6B68F6f5FE32677585ECc"
+const nftaddress = "0xe7Ac27ac4E6C1Fa70aaa33FA28cCaf0f1b7156B8"
+
+module.exports = { nftaddress, nftmarketaddress }
+  
